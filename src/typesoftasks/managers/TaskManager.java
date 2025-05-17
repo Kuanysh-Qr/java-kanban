01868509e35b259e33629e2fba6a0f9d776947d7
Kuanysh-Qr <k.kurmankul@yandex.kz>
2025-05-17 23:03:49 +0500

    Исправлен ошибка TaskManager

--- src/typesoftasks/managers/TaskManager.java
+++ src/typesoftasks/managers/TaskManager.java
@@ -15,19 +15,27 @@ public interface TaskManager {
     Subtask getSubtaskById(int id);
 
     Task getTask(int id);
+
     Epic getEpic(int id);
 
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
 
     List<Task> getAllTasks();
+
     Collection<Epic> getAllEpics();
+
     Collection<Subtask> getAllSubtasks();
+
     List<Subtask> getSubtasksByEpic(int epicId);
 
     void deleteTaskById(int id);
+
     void deleteEpicById(int id);
+
     void deleteSubtaskById(int id);
 
     List<Task> getHistory();
