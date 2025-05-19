211852707840082bdaf42600a000ab9144a9a331
Kuanysh-Qr <k.kurmankul@yandex.kz>
2025-05-19 21:09:39 +0500

    TaskManager исправление Checkstyle

--- src/typesoftasks/managers/TaskManager.java
+++ src/typesoftasks/managers/TaskManager.java
@@ -1,32 +1,34 @@
-package typesoftasks.managers;
-
-import typesoftasks.tasks.Epic;
-import typesoftasks.tasks.Subtask;
-import typesoftasks.tasks.Task;
-
-import java.util.Collection;
-import java.util.List;
-
 public interface TaskManager {
     Task createTask(String title, String description);
+
     Epic createEpic(String title, String description);
+
     Subtask createSubtask(String title, String description, int epicId);
+
     Subtask getSubtaskById(int id);
 
     Task getTask(int id);
+
     Epic getEpic(int id);
 
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
 
     List<Task> getAllTasks();
+
     Collection<Epic> getAllEpics();
+
     Collection<Subtask> getAllSubtasks();
+
     List<Subtask> getSubtasksByEpic(int epicId);
 
     void deleteTaskById(int id);
+
     void deleteEpicById(int id);
+
     void deleteSubtaskById(int id);
 
     List<Task> getHistory();
