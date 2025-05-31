9eabbb038a8209b05e439c87d03db5aa9c3cf142
Kuanysh-Qr <k.kurmankul@yandex.kz>
2025-05-31 17:10:42 +0500

    Исправлены конфликты и обновлены файлы

--- src/typesoftasks/tasks/Epic.java
+++ src/typesoftasks/tasks/Epic.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Epic extends Task {
+
     private final List<Integer> subtaskIds;
 
     public Epic(int id, String title, String description) {
